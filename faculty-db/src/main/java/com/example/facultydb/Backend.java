9f09f2c0323d4afd658330443b5c9975dd9eff8c
Ritvik Pandillapally <ritvik.19bcn7117@vitap.ac.in>
2022-04-22 19:37:17 -0400

    added functions for the recent phase

--- faculty-db/src/main/java/com/example/facultydb/Backend.java
+++ faculty-db/src/main/java/com/example/facultydb/Backend.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 public class Backend {
     private Connection conn;
-    private Statement stmt;
     private ResultSet rs;
     private String query;
 
@@ -28,6 +27,7 @@ public class Backend {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(url, userName, password);
             System.out.println("\nCreated Connection!\n");
+
         } catch (ClassNotFoundException cnfe) {
             System.out.println("ERROR, CAN NOT CONNECT!!");
             System.out.println("Class");
@@ -71,25 +71,10 @@ public class Backend {
 
     // insert into abstract table
     public void insertAbstract(String abs) {
-        int rows = 0;
         try {
             query = "INSERT INTO abstract(abstract) VALUES(?)";
             PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setString(1, abs);
-            rows = pstmt.executeUpdate();
-        } catch (SQLException sqle) {
-            System.out.println(sqle);
-        }
-    }
-
-    // get all the abstract values for the faculty to insert into the select box
-    public void getAllAbstract() {
-        try {
-            query = "SELECT * FROM abstract";
-            rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
         } catch (SQLException sqle) {
             System.out.println(sqle);
         }
@@ -143,7 +128,7 @@ public class Backend {
         return id;
     }
 
-    // get all the information of the faculty
+    // get all the information of the faculty from student
     public ArrayList<String> getAllFacultyInfo(int id) {
         try{
             ArrayList<String> info = new ArrayList<>();
@@ -166,8 +151,166 @@ public class Backend {
             System.out.println(sqle);
             return null;
         }
+    }
 
+//    student/guest searches faculty by name
+    public ArrayList<String> getFacultyByName(String name) {
+        try{
+            ArrayList<String> info = new ArrayList<>();
+            String query = "SELECT faculty.id AS \"Faculty ID\",name, email, room, phone, abstract.abstract AS \"Abstract\", office.hours AS \"Hours\" FROM faculty INNER JOIN faculty_has_abstract ON faculty.id = faculty_has_abstract.faculty_id INNER JOIN abstract ON faculty_has_abstract.abstract_id = abstract.id INNER JOIN faculty_has_office ON faculty.id = faculty_has_office.faculty_id INNER JOIN office ON faculty_has_office.office_id = office.id WHERE name=? ORDER BY name;";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                info.add(rs.getString(2));
+                info.add(rs.getString(3));
+                info.add(rs.getString(4));
+                info.add(rs.getString(5));
+                info.add(rs.getString(6));
+                info.add(rs.getString(7));
+            }
+            return info;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return null;
+        }
+    }
+
+//    faculty searches student by interest (regex)
+    public ArrayList<String> getStudentByInterest(String interest) {
+        try{
+            ArrayList<String> info = new ArrayList<>();
+            String query = "SELECT student.id AS \"Student ID\", name, email, major, minor, gpa, phone, interest FROM student INNER JOIN student_has_interest ON student.id = student_has_interest.student_id WHERE interest LIKE ? ORDER BY name;";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, interest);
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                info.add(rs.getString(2));
+                info.add(rs.getString(3));
+                info.add(rs.getString(4));
+                info.add(rs.getString(5));
+                info.add(rs.getString(6));
+                info.add(rs.getString(7));
+                info.add(rs.getString(8));
+            }
+            return info;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return null;
+        }
+    }
+
+//    guest search faculty by abstract
+    public ArrayList<String> getFacultyByAbstract(String abstractText) {
+        try{
+            ArrayList<String> info = new ArrayList<>();
+            String query = "SELECT faculty.id AS \"Faculty ID\",name, email, room, phone, abstract.abstract AS \"Abstract\", office.hours AS \"Hours\" FROM faculty INNER JOIN faculty_has_abstract ON faculty.id = faculty_has_abstract.faculty_id INNER JOIN abstract ON faculty_has_abstract.abstract_id = abstract.id INNER JOIN faculty_has_office ON faculty.id = faculty_has_office.faculty_id INNER JOIN office ON faculty_has_office.office_id = office.id WHERE abstract.abstract LIKE ? ORDER BY name;";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, abstractText);
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                info.add(rs.getString(2));
+                info.add(rs.getString(3));
+                info.add(rs.getString(4));
+                info.add(rs.getString(5));
+                info.add(rs.getString(6));
+                info.add(rs.getString(7));
+            }
+            return info;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return null;
+        }
+    }
+
+//    show interested guests to faculty
+    public ArrayList<String> getInterestedGuests(String facultyID) {
+        try{
+            ArrayList<String> info = new ArrayList<>();
+            String query = "SELECT guest.id AS \"Guest ID\", name, email, phone, interest FROM guest INNER JOIN guest_has_interest ON guest.id = guest_has_interest.guest_id INNER JOIN faculty_has_interest ON guest_has_interest.interest_id = faculty_has_interest.interest_id WHERE faculty_has_interest.faculty_id = ? ORDER BY name;";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, facultyID);
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                info.add(rs.getString(2));
+                info.add(rs.getString(3));
+                info.add(rs.getString(4));
+                info.add(rs.getString(5));
+            }
+            return info;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return null;
+        }
+    }
+
+//    create guest
+    public boolean createGuest(String name, String email, String phone) {
+        try{
+            String query = "INSERT INTO guest (name, email, phone) VALUES (?,?,?);";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.executeUpdate();
+            return true;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return false;
+        }
+    }
+
+//    Insert faculty interest of guest
+    public boolean insertFacultyInterest(String guestID, String interestID) {
+        try{
+            String query = "INSERT INTO guest_has_interest (guest_id, interest_id) VALUES (?,?);";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, guestID);
+            preparedStatement.setString(2, interestID);
+            preparedStatement.executeUpdate();
+            return true;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return false;
+        }
+    }
+
+//    update faculty
+    public boolean updateFaculty(String id, String name, String email, String phone) {
+        try{
+            String query = "UPDATE faculty SET name = ?, email = ?, phone = ? WHERE id = ?;";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, id);
+            preparedStatement.executeUpdate();
+            return true;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return false;
+        }
+    }
+
+//    delete faculty
+    public boolean deleteFaculty(String id) {
+        try{
+            String query = "DELETE FROM faculty WHERE id = ?;";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, id);
+            preparedStatement.executeUpdate();
+            return true;
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+            return false;
+        }
     }
 }
-
-
